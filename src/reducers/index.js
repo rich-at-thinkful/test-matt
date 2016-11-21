229673ddb5ce93a95db8bcd812e4527136e7d80a
mat <reallymathew@gmail.com>
2016-11-21 16:53:52 -0600

    fix conflicts in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { handleActions } from 'redux-actions';
 import * as actions from '../actions/index';
 
@@ -7,15 +6,6 @@ const initialState = {
     currentUsername: null,
     currentPassword: null
 }
-=======
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-
-// import './config';
-// import App from './components/App';
-// import './index.css';
->>>>>>> components
 
 export default handleActions({
     [actions.SET_SIGN_IN]: (state, action) => {
