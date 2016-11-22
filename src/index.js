826ed6539861316bc1f349ee022d6fe7fc62748a
KL012 <kevl927@gmail.com>
2016-11-22 00:09:43 +0000

    fix whitelisting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
@@ -26,4 +27,34 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+fetch('https://polar-escarpment-86427.herokuapp.com/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'tester1',
+        password: 'tester1'
+      })
+    }).then(response => response.json().then(json => ({ json, response })))
+      .then(({json, response}) => {
+      if (response.ok === false) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+    .then(
+      data => {
+    console.log(data);
+      },
+      ({response, data}) => {
+          console.log(data);
+          
+          if(response.status == 401) {
+              console.log(data.error);
+          }
+      }
+    );
+    
 export default store;
